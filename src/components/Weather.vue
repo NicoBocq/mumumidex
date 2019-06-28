7cdbb96ca1f5c3de6a77920c094388b69ba15db9
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-06-29 00:53:25 +0200

    Done

---
+++ src/components/Weather.vue
@@ -0,0 +1,115 @@
+<template>
+  <div v-if="show">
+    <!-- <h1>{{ items.location.name }}</h1> -->
+     <!-- <p>{{ items.current.feelslike_c }}</p>
+     <p>{{ items.current.last_updated }}</p>
+      -->
+      <table>
+              <caption>MumUMidex Challenge</caption>
+
+        <tr
+          v-for="(item, ObjKey) in orderedTemp"
+          :key="item.id"
+        >
+        <td>
+          {{ObjKey + 1}}
+        </td>
+        <td>
+          {{item.location.name}}
+        </td>
+        <td>
+          {{item.current.feelslike_c}} ({{item.current.temp_c}})
+        </td>  
+        <td>
+          <span class="updated">{{item.current.last_updated}}</span>
+        </td>
+        </tr>
+      </table>
+      
+  </div>
+</template>
+
+<script>
+
+const cities = ['Marseille', 'Valence', 'Paris', 'Montpellier', 'Miami', 'San Francisco', 'Lyon']
+// const apiUrl = 'http://localhost:3000/current'
+const apiUrl = "https://api.apixu.com/v1/current.json?key=166fba8a30324b87be8203006192606&q="
+
+import axios from 'axios';
+import lodash from 'lodash';
+
+export default {
+  data () {
+    return {
+      items:[],
+      show: false
+    } 
+  },
+  computed: {
+    orderedTemp: function () {
+      return _.orderBy(this.items, ['current.feelslike_c'], ['desc'])
+    }
+  },
+  mounted () {
+  //   function getApi() {
+  //     cities.forEach(function(element) {
+  //       return axios.get(apiUrl + cities[element]);
+  //   });
+  //   this.items = getApi();
+  //   }
+  // }
+  // axios.all([getApi()])
+  //   .then(axios.spread(function (acct, perms) {
+  //     // response => (this.items = response.data)
+  //   }));
+
+    const promises = [];
+    var myUrl;
+
+    cities.forEach(function(element){
+      myUrl = apiUrl+'?'+element;
+      promises.push(axios.get(myUrl))
+    });``
+    let self = this;
+
+    axios
+      .all(promises)
+      .then(axios.spread((...responses) => {
+        responses.forEach(res => self.items.push(res.data))
+        // self.items = responses.data
+        // // response => (this.items = response.data)
+        self.show = true
+    }));
+    // axios.all(promises).then(function(results) {
+    //     results.forEach(function(response) {
+    //         mainObject[response.identifier] = response.data;
+    //     })
+    // });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  width: 100%;
+  background-color:#fff;
+  padding: 1rem;
+  margin: 1rem;
+  color: indianred;
+}
+caption {
+  background:#FFF;
+  font-size:3rem;
+  color: indianred;
+}
+td {
+    padding:1rem;
+      font-size:2em;
+}
+
+span.updated {
+  font-size:0.6em;
+  }
+
+</style>
