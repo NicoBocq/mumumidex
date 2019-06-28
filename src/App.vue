31841f6973ed6719ecd0b7165f0fdf3c61a76efd
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-06-29 00:50:38 +0200

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Logo  /> -->
+    <Weather />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Weather from './components/Weather.vue'
+import Logo from './components/Logo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Weather,
+    Logo
   }
 }
 </script>
 
 <style>
+body {
+    padding:0;
+    margin:0;
+    background-color: indianred;
+    background-image: url(./assets/background.gif);
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+
 }
+
 </style>
