fed6fdf49b2d40ec554866295bf5ff844a7d9fa4
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-07-03 00:23:52 +0200

    Add icon

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+ <footer>
+  V 1 - <a href="https://github.com/NicoBocq" target="-blank">
+   @NicoBocq
+  </a>
+ </footer>
+</template>
+
+<style scoped>
+footer {
+ background-color: rgba(O,O,O,O.7);
+ font-size:0.9em;
+ text-align: right;
+ position: absolute;
+ bottom: 0;
+ right: 0;
+ padding: 0.5rem;
+margin-right: 0.5rem;
+}
+
+footer a {
+ color: #FFF;
+}
+</style>
