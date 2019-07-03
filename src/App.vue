774a265dbcf38b44682fab51777b8dc1edc2ef5b
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-07-03 17:31:28 +0200

    Add refresh button

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <button
+      v-if="updateExists"
+      @click="refreshApp"
+    >
+      New version available! Click to update
+    </button>
     <Header />
     <Weather />
     <Footer />
@@ -18,7 +24,42 @@ export default {
     Weather,
     Footer,
     Header
-  }
+  },
+  data () {
+    return {
+      refreshing: false,
+      registration: null,
+      updateExists: false,
+    };
+  },
+  
+  created () {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener('swUpdated', this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      if (this.refreshing) return;
+      this.refreshing = true;
+      window.location.reload();
+    });
+  },
+  methods: {
+    showRefreshUI (e) {
+      // Display a button inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      this.registration = e.detail;
+      this.updateExists = true;
+    },
+    refreshApp () {
+      // Handle a user tap on the update app button.
+      this.updateExists = false;
+      // Protect against missing registration.waiting.
+      if (!this.registration || !this.registration.waiting) { return; }
+      this.registration.waiting.postMessage('skipWaiting');
+    },
+  },
 }
 </script>
 
