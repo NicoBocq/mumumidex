1ae7f8e5c69f5f327c4d8c79487637663dd8c827
Nicolas Bocquet <nicolas.fow@gmail.com>
2019-09-14 23:27:25 +0200

    Init vuex

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store/store'
+
 import './registerServiceWorker'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
@@ -18,5 +20,6 @@ Vue.use(Vuetify, {
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
